151f852b30bc3874558c772d2052ef5a40c22b1f
Hipolito <hipolitogomes@ufba.br>
2025-10-16 09:26:33 -0300

    exercise 2

--- src/exercicoIncremental1/AluguelCarro.java
+++ src/exercicoIncremental1/AluguelCarro.java
@@ -2,6 +2,7 @@ package exercicoIncremental1;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 public class AluguelCarro {
     private String cidadeDestino;
@@ -9,27 +10,73 @@ public class AluguelCarro {
     private LocalDate dataFinal;
     private LocalTime horaRetirada;
     private LocalTime horaDevolucao;
-    private char categoria;
+    private double km;
 
-    public AluguelCarro(String cidadeDestino, LocalDate datasInicial, LocalDate dataFinal, LocalTime horaRetirada, LocalTime horaDevolucao){
+    public AluguelCarro(Carro categoria, String cidadeDestino, LocalDate datasInicial, LocalDate dataFinal, LocalTime horaRetirada, LocalTime horaDevolucao){
         this.cidadeDestino = cidadeDestino;
         this.datasInicial = datasInicial;
         this.dataFinal = dataFinal;
         this.horaRetirada = horaRetirada;
         this.horaDevolucao = horaDevolucao;
+        this.km = 200;
     }
-    public AluguelCarro(String cidadeDestino, LocalDate datasInicial, LocalDate dataFinal, LocalTime horaRetirada){
+    public AluguelCarro(Carro categoria, String cidadeDestino, LocalDate datasInicial, LocalDate dataFinal, LocalTime horaRetirada km){
         this.cidadeDestino = cidadeDestino;
         this.datasInicial = datasInicial;
         this.dataFinal = dataFinal;
         this.horaRetirada = horaRetirada;
         this.horaDevolucao = horaRetirada;
+        this.km = km;
     }
 
     public double ValorAlocacao(LocalDate datasInicial, LocalDate dataFinal, char categoria){
+        int a = 350;
+        int b = 250;
+        int c = 150;
+        double valor;
+        long dias = ChronoUnit.DAYS.between(this.dataFinal,this.datasInicial);
         if (categoria =='A'){
-
+            valor = 350 * dias;
         }
+        else if (categoria =='B'){
+            valor = 250 * dias;
+        }
+        else if (categoria =='C'){
+            valor  = 150 * dias;
+        }
+        return valor;
+    }
+    public long KmAcumulado(LocalDate datasInicial, LocalDate dataFinal){
+        long dias = ChronoUnit.DAYS.between(this.dataFinal,this.datasInicial);
+        long total = 200 * dias;
+        return total;
+    }
+    public double KmAdicional(double km, char categoria){
+        double valor;
+        if (categoria =='A'){
+            valor = 0.3 * km;
+        }
+        else if (categoria =='B'){
+            valor = 0.25 * km;
+        }
+        else if (categoria =='C'){
+            valor  = 0.2 * km;
+        }
+    }
+
+    public double ilimitada(LocalDate datasInicial, LocalDate dataFinal, char categoria){
+        double valor;
+        long dias = ChronoUnit.DAYS.between(this.dataFinal,this.datasInicial);
+        if (categoria =='A'){
+            valor = 70 * dias;
+        }
+        else if (categoria =='B'){
+            valor = 60 * dias;
+        }
+        else if (categoria =='C'){
+            valor = 50 * dias;
+        }
+
     }
 
     public String getCidadeDestino() {
--- src/exercicoIncremental1/Carro.java
+++ src/exercicoIncremental1/Carro.java
@@ -1,4 +1,36 @@
 package exercicoIncremental1;
-
+// por enquanto não é necessario a classe,
 public class Carro {
+    private char categoria;
+    private double km;
+
+
+    public Carro(char categoria, double valorLocacao){
+        this.categoria = categoria;
+        this.km = 200;
+        this.valorLocacao = valorLocacao;
+    }
+    public Carro(char categoria, double km, double valorLocacao){
+        this.categoria = categoria;
+        this.km = 200;
+        this.valorLocacao = valorLocacao;
+    }
+    public void setCategoria(char categoria){
+        this.categoria = categoria;
+    }
+    public char getCategoria(){
+        return this.categoria;
+    }
+    public void setKm(double km){
+        this.km = km;
+    }
+    public void getKm(){
+        return this.km;
+    }
+    public void setValorLocacao(double valorLocacao){
+        this.valorLocacao = valorLocacao;
+    }
+    public double getValorLocacao(){
+        return this.valorLocacao;
+    }
 }
